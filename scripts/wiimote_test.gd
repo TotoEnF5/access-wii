975aae32c99076886823b87985e1745bf3f34ca7
TotoEnF5 <totooodev@proton.me>
2026-09-07 14:57:52 +0200

    add wiimote rumble

--- scripts/wiimote_test.gd
+++ scripts/wiimote_test.gd
@@ -29,3 +29,8 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	for wiimote in GDWiimoteServer.get_connected_wiimotes():
+		wiimote.toggle_rumble()
