08fcdfe7b101bd59ae91a4c61567a6dfd9bc7dba
TotoEnF5 <totooodev@proton.me>
2026-09-07 15:41:22 +0200

    add motion sensing

--- scripts/wiimote_test.gd
+++ scripts/wiimote_test.gd
@@ -10,6 +10,12 @@ func _ready() -> void:
 	thread.start(_connect_wiimotes_thread)
 
 
+func _process(delta: float) -> void:
+	for wiimote in GDWiimoteServer.get_connected_wiimotes():
+		$Label2.text = str(wiimote.get_processed_accel())
+		break
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("player1_slash"):
 		$Label.text = "player 1 pressed A"
@@ -24,6 +30,9 @@ func _connect_wiimotes_thread() -> void:
 
 func _on_button_pressed() -> void:
 	GDWiimoteServer.finalize_connection()
+	for wiimote in GDWiimoteServer.get_connected_wiimotes():
+		wiimote.set_motion_processing(true)
+		wiimote.set_motion_sensing(true)
 	$Label.text = "connected!"
 
 
